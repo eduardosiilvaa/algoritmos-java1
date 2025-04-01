f299dca7ae242666357f1345fbcaeae3675c8007
eduardosiilvaa <eduardojunio2004@gmail.com>
2025-04-01 16:52:01 -0300

    Update Exercicio8.java

--- Exercicio8.java
+++ Exercicio8.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class Exercicio8 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Solicitar os dados
-        System.out.print("Informe o número total de veículos no estacionamento: ");
-        int totalVeiculos = scanner.nextInt();
-
-        System.out.print("Informe o total de rodas no estacionamento: ");
-        int totalRodas = scanner.nextInt();
-
-        // Calcular a quantidade de carros e motos
-        int carros = (totalRodas - totalVeiculos * 2) / 2;
-        int motos = totalVeiculos - carros;
-
-        // Exibir o resultado
-        System.out.println("Carros: " + carros);
-        System.out.println("Motos: " + motos);
-
-        scanner.close();
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+      // Aqui mostra o total de quantos carros e motos há no estacionamento
+      System.out.print("Digite o total de veículos: ");
+      int totalVeiculos = scanner.nextInt();
+      
+      System.out.print("Digite o total de rodas: ");
+      int totalRodas = scanner.nextInt();
+      
+      int carros = (totalRodas - 2 * totalVeiculos) / 2;
+      int motos = totalVeiculos - carros;
+      
+      // Exibe o resultado
+      System.out.println("Total de carros: " + carros);
+      System.out.println("Total de motos: " + motos);
+      
+      scanner.close();
+  }
 }
